d4923eabea00835ade0beede8ee4c01482a653ab
serhatcetinkaya <serhatcetinkaya@pm.me>
2020-02-04 12:37:25 +0300

    add quick-ssh-keygen

---
+++ quick-ssh-keygen/gen-key.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -euf -o pipefail
+
+GREEN='\033[0;32m'
+RED='\033[0;33m'
+NC='\033[0m'
+
+TEMPDIR=$(mktemp -d)
+
+printf "${GREEN}Generating keys:${NC}\n"
+ssh-keygen -N "" -C "" -f ${TEMPDIR}/key
+
+printf "\n${GREEN}Private key:${NC}\n"
+cat ${TEMPDIR}/key
+
+printf "\n${GREEN}Public key:${NC}\n"
+cat ${TEMPDIR}/key.pub
+
+printf "\n${GREEN}Private key with removed newlines:${NC}\n"
+awk '{printf "%s\\n", $0}' ${TEMPDIR}/key
+
+rm -rf $TEMPDIR
+printf "\n\n${RED}Removed keys from filesystem successfully${NC}\n"
